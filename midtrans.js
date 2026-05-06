327ce7bd8af87320744c50023f131df90b24c1be
Richeese4 <78680000+Richeese4@users.noreply.github.com>
2026-05-06 10:26:11 +0700

    Update midtrans.js

--- midtrans.js
+++ midtrans.js
@@ -2,7 +2,7 @@ require("dotenv").config()
 const midtransClient = require("midtrans-client")
 
 const snap = new midtransClient.Snap({
-  isProduction: process.env.MIDTRANS_IS_PRODUCTION === "true",
+  isProduction: false,
   serverKey: process.env.MIDTRANS_SERVER_KEY,
   clientKey: process.env.MIDTRANS_CLIENT_KEY
 })
